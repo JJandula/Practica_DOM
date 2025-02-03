1d4bbec812af13d60e2a11d5a6cf11a26a756d9f
JJandula <joseantify@gmail.com>
2025-02-03 16:29:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const products = [
         name: 'SWANS HOODIE GREY MELANGE',
         price: 89,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/SWANS_HOOD_GREY_MELANGE_back1_1200x.jpg?v=1729759821',
         image2: 'https://nude-project.com/cdn/shop/files/ProCam-IMG-1016.jpg?v=1728897297'
     },
@@ -11,7 +15,11 @@ const products = [
         name: 'SWANS HOODIE BLACK',
         price: 89,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/SWAN_HOOD_BLACK_back_1200x.jpg?v=1727271159',
         image2: 'https://nude-project.com/cdn/shop/files/IPHONE1080-9324_800x.jpg?v=1734445294'
     },
@@ -19,7 +27,11 @@ const products = [
         name: 'GRADIENT ZIP-UP HOODIE PINK',
         price: 129,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/GRADIENT_PINK_FRONT_cb0b5e79-500e-48e1-8578-b610e12cfffa_1200x.jpg?v=1733415592',
         image2: 'https://nude-project.com/cdn/shop/files/IMG_6337_800x.png?v=1735056509'
     },
@@ -27,7 +39,11 @@ const products = [
         name: 'GRADIENT ZIP-UP HOODIE BROWN',
         price: 129,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/VOLA_front_0154fac8-8e69-45d0-a6af-b717a5fc87d0_1200x.jpg?v=1725525293',
         image2: 'https://nude-project.com/cdn/shop/files/ffffsa_800x.png?v=1726160100'
     },
@@ -35,7 +51,11 @@ const products = [
         name: 'GRADIENT COZY SWEATER BABY BLUE',
         price: 119,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/GRADIENT_COZY_SWEATER_BROWN_front_1200x.jpg?v=1732196046',
         image2: 'https://nude-project.com/cdn/shop/files/IPHONE1080-2797_800x.jpg?v=1734630030'
     },
@@ -43,7 +63,11 @@ const products = [
         name: 'GRADIENT COZY SWEATER ORANGE',
         price: 119,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/GRADIENT_COZY_SWEATERORANGE_FRONT_1_1200x.jpg?v=1733410263',
         image2: 'https://nude-project.com/cdn/shop/files/IMG_4548.png?v=1735055946'
     },
@@ -51,7 +75,11 @@ const products = [
         name: 'GRADIENT COZY SWEATER RED',
         price: 119,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/GRADIENT_COZY_SWEATER_RED_front_b67b432b-790f-4f87-9c9b-b1e9547d043c_1200x.jpg?v=1732200136',
         image2: 'https://nude-project.com/cdn/shop/files/IPHONE1080-0696.jpg?v=1727283848'
     },
@@ -59,7 +87,11 @@ const products = [
         name: 'FUCK NORMAL HOODIE GREY MELANGE',
         price: 79,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/FUCK_NORMAL_HOOD_GREY_back_1200x.jpg?v=1729782302',
         image2: 'https://nude-project.com/cdn/shop/files/IMG_2102.png?v=1730377699'
     },
@@ -67,7 +99,11 @@ const products = [
         name: 'FUCK NORMAL HOODIE BLACK',
         price: 79,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/FUCK_NORMAL_HOOD_BLACK_back_1200x.jpg?v=1731489565',
         image2: 'https://nude-project.com/cdn/shop/files/IMG_4650_800x.jpg?v=1732209048'
     },
@@ -75,7 +111,11 @@ const products = [
         name: 'FUCK NORMAL HOODIE NAVY',
         price: 79,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/NORMAL_HOOD_NAVY_back_1200x.jpg?v=1729682700',
         image2: 'https://nude-project.com/cdn/shop/files/IMG_1374_bc9c18ae-6182-4817-9e37-72c902458261.png?v=1732208219'
     },
@@ -83,7 +123,11 @@ const products = [
         name: 'OG PUFFER JACKET BLACK',
         price: 219,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/REAL_DOWN_PUFFER_BLACK_front_800x1000_d1efb429-3a54-40c4-b1e2-62f40408320c_1200x.jpg?v=1730996552',
         image2: 'https://nude-project.com/cdn/shop/files/BLACK1.png?v=1731057756&s'
     },
@@ -91,7 +135,11 @@ const products = [
         name: 'OG PUFFER JACKET GREY',
         price: 219,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/REAL_DOWN_PUFFER_GRAY_front_1_800x1000_8fd3f53a-6e6c-4a2e-83e6-5b08b1f50110_1200x.jpg?v=1730997168',
         image2: 'https://nude-project.com/cdn/shop/files/R1-03358-0010_800x.png?v=1730970377'
     },
@@ -99,7 +147,11 @@ const products = [
         name: 'OG PUFFER JACKET MARSHMALLOW',
         price: 219,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/REAL_DOWN_PUFFER_IVORY_front_1_800x1000_16ddf021-5bdc-4640-8b6f-66810a834612_1200x.jpg?v=1730997091',
         image2: 'https://nude-project.com/cdn/shop/files/R1-03362-0032_800x.png?v=1730970681'
     },
@@ -107,7 +159,11 @@ const products = [
         name: 'OG PUFFER JACKET BLUE',
         price: 219,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/REAL_DOWN_PUFFER_BLUE_front_1_800x1000_7a41e477-6001-4e59-b86b-016f064ed519_1200x.jpg?v=1730997029',
         image2: 'https://nude-project.com/cdn/shop/files/BLUE1_800x.png?v=1731057720'
     },
@@ -115,7 +171,11 @@ const products = [
         name: 'LE MARAIS KNIT GREY',
         price: 109,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/LE_MARAIS_KNIT_WHITE_FRONT_1200x.jpg?v=1725986259',
         image2: 'https://nude-project.com/cdn/shop/files/IMG_4180_800x.png?v=1725987113'
     },
@@ -123,7 +183,11 @@ const products = [
         name: 'LE MARAIS KNIT BROWN',
         price: 109,
         seller: 'Nude Project',
-        sizes : '<p><span class="no-space">XS</span></p><p>S</p> <p>M</p> <p>L</p> <p><span class="no-space">XL</span></p>',
+        sizeXs: 'XS',
+        sizeS: 'S',
+        sizeM: 'M',
+        sizeL: 'L',
+        sizeXl: 'XL',
         image: 'https://nude-project.com/cdn/shop/files/LE_MARAIS_KNIT_BROWN_FRONT_1200x.jpg?v=1725986254',
         image2: 'https://nude-project.com/cdn/shop/files/LE_MARAIS_KNIT_BROWN_BACK.jpg?v=1725986254'
     },
@@ -147,7 +211,11 @@ const products = [
         <p class="products-specs">€${product.price}</p>
         <p class="products-specs">Vendido por: ${product.seller}</p>
         <div class="sizes-products">
-        ${product.sizes}
+          <p><span class="no-space">${product.sizeXs}</span></p>
+          <p>${product.sizeS}</p>
+          <p>${product.sizeM}</p>
+          <p>${product.sizeL}</p>
+          <p><span class="no-space">${product.sizeXl}</span></p>
         </div>
         `
 
